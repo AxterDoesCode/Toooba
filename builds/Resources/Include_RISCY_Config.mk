922e50be563af0289a6315691d0654f19344a282
Jonathan Woodruff <jonwoodruff@gmail.com>
2026-05-15 15:39:45 +0100

    Configure with upstream caches and the selective CDP prefetcher.

--- builds/Resources/Include_RISCY_Config.mk
+++ builds/Resources/Include_RISCY_Config.mk
@@ -10,7 +10,7 @@ PROC := RV64G_OOO
 # core size
 CORE_SIZE ?= SMALL
 # cache size
-CACHE_SIZE ?= ALEX_SMALLISH
+CACHE_SIZE ?= LARGE
 # always include perf counter
 PERF_COUNT := true
 # dram type in simulation: VC707 or AWSF1
@@ -48,7 +48,7 @@ RENAME_DEBUG ?= false
 INSTR_PREFETCHER_LOCATION ?= NONE
 INSTR_PREFETCHER_TYPE ?= SINGLE_WINDOW_TARGET
 DATA_PREFETCHER_LOCATION ?= L1
-DATA_PREFETCHER_TYPE ?= CDP
+DATA_PREFETCHER_TYPE ?= SELECTIVECDP
 
 # clk frequency depends on core size
 ifneq (,$(filter $(CORE_SIZE),TINY SMALL BOOM MEDIUM))
